e79d862d7fa8c3f56765c25d83ed1a48c870fb78
xingyue <1716265400@qq.com>
2020-03-13 11:54:29 +0800

    增加手写javawrite.java

---
+++ iInterface-master/src/test/java/com/hogwarts/testcase/javawrite.java
@@ -0,0 +1,13 @@
+package com.hogwarts.testcase;
+
+public class javawrite {
+    double s = 2.34;
+    public static void main(String[]args){
+        javawrite tester=new javawrite();
+        tester.test1();
+    }
+    public void test1(){
+        System.out.println("====测试使用======");
+    }
+
+}
