e51fe13eaa041b90f36ad6b36d6076af7c5c3a25
xingyue <1716265400@qq.com>
2020-03-13 15:31:59 +0800

    增加calss Dog.java

--- iInterface-master/src/test/java/com/hogwarts/testcase/javawrite.java
+++ iInterface-master/src/test/java/com/hogwarts/testcase/javawrite.java
@@ -1,13 +1,24 @@
 package com.hogwarts.testcase;
-
+/*auter:xingye
+time :2020-3-13*/
 public class javawrite {
     double s = 2.34;
     public static void main(String[]args){
         javawrite tester=new javawrite();
         tester.test1();
+        tester.test2();
     }
     public void test1(){
         System.out.println("====测试使用======");
+    }
+    public void test2(){
+        int mimiage;
+        String color;
+        String elema;
+        Dog mimi=new Dog();
+        mimiage=mimi.age();
+        color=mimi.color();
+        System.out.println("mimi is "+mimiage+" years old");
     }
 
 }
