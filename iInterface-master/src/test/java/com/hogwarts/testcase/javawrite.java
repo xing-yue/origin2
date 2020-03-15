91d69fc74ec55dd0f92d82d7f2fbb132f735be99
xingyue <1716265400@qq.com>
2020-03-15 11:52:19 +0800

    增加连接JDBC查询和插入数据

--- iInterface-master/src/test/java/com/hogwarts/testcase/javawrite.java
+++ iInterface-master/src/test/java/com/hogwarts/testcase/javawrite.java
@@ -1,4 +1,8 @@
 package com.hogwarts.testcase;
+
+import java.text.ParseException;
+import java.util.concurrent.Callable;
+
 /*auter:xingye
 time :2020-3-13*/
 public class javawrite {
@@ -7,24 +11,47 @@ public class javawrite {
         javawrite tester=new javawrite();
         tester.test1();
         tester.test2();
+        tester.test3();
     }
+
+    private void test3() {
+        ConSql conSql;
+        Object[] data ={"xihuan","32","3","4","5","6","7"};
+        conSql = new ConSql();
+        try {
+            conSql.saveinfo(data);
+            conSql.findinfo(data);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println("do it ");
+    }
+
     public void test1(){
         System.out.println("====测试使用======");
     }
     public void test2(){
         int mimiage;
         String color;
-        String elema;
         String ishun;
         Dog mimi;
         int[]nums={1,2,3,4};
         for(int a2:nums){
         mimi = new Dog(a2,"blue" );
-        mimiage=mimi.age();
+        mimiage=mimi.age;
         color=mimi.color();
         ishun=mimi.ishun();
+
         System.out.println("mimi is "+mimiage+" years old,and it is "+color);
+        if(a2==2){
+            continue;
+        }
+            if(a2==3){
+                break;
+            }
         }
     }
 
 }
+
+
